c738b03465275a14f3f72eb8b31298649cad69c1
Artem <muha13artem@gmail.com>
2026-02-03 14:53:17 +0200

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,25 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const arr = this;
+    const len = arr.length;
+
+    // Simple bubble sort implementation
+    for (let i = 0; i < len - 1; i++) {
+      for (let j = 0; j < len - i - 1; j++) {
+        const shouldSwap = compareFunction
+          ? compareFunction(arr[j], arr[j + 1]) > 0
+          : String(arr[j]) > String(arr[j + 1]);
+
+        if (shouldSwap) {
+          // Swap arr[j] and arr[j + 1]
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+    }
+
+    return arr;
   };
 }
 
